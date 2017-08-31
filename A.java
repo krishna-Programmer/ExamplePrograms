13aa9762ce4787bafea33249db4e6d34cf6fbd07
Krishna <krishnarao009@gmail.com>
2017-08-31 14:11:35 +0000

    Update A.java

--- A.java
+++ A.java
@@ -1,3 +1,15 @@
+/* 
+This program takes a number as range and from 1 to that range if the numbers are 
+represented in binary then how many 1's does all these numbers contains that will be printed as result
+
+for example 
+range is 4
+then binary form of 1 contains 1 1's
+binary form of 2 contains 1 1's
+binary form of 3 contains 2 1's
+binary form of 4 contains 1 1's
+so the output will be 1+1+2+1=5
+*/
 import java.util.*;
 class A
 {
